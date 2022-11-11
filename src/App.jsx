c38ce59a740cc486877d09fb32c59d61cdf4c364
Victor Mendez <victor.mmc.pl@hotmail.com>
2022-11-10 21:34:16 -0600

    Implemented Card component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import './App.css'
+import Card from './components/Card'
+import ProductMobile from "../images/image-product-mobile.jpg";
 
 function App() {
   return (
     <div className="App">
+      <Card
+      img={ProductMobile}
+      type='perfume'
+      title='Gabrielle Essence Eau De Parfum'
+      description='A floral solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for th eHouse of CHANEL'
+      originalPrice={169.99}
+      discountedPrice={149.99}
+      available={true}
+      />
     </div>
   )
 }
